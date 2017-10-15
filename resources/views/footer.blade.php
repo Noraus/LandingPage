3c13b1ca641768c338f24f44c683e4162a5b9399
noraus <ckukp0m8>
2017-10-15 22:37:02 +0200

    Holaa

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,10 +3,10 @@
         <hr />
         <div class="text-center center-block">
             <p class="txt-railway">- Copyright eleclockers.com -</p>
-            <a href="https://www.facebook.com/bootsnipp"><i class="fa fa-facebook-square fa-3x social"></i></a>
-            <a href="https://twitter.com/bootsnipp"><i class="fa fa-twitter-square fa-3x social"></i></a>
-            <a href="https://plus.google.com/+Bootsnipp-page"><i class="fa fa-google-plus-square fa-3x social"></i></a>
-            <a href="mailto:bootsnipp@gmail.com"><i class="fa fa-envelope-square fa-3x social"></i></a>
+            <a href="https://www.facebook.com/eleclockers"><i class="fa fa-facebook-square fa-3x social"></i></a>
+            <a href="https://twitter.com/eleclockers"><i class="fa fa-twitter-square fa-3x social"></i></a>
+            <a href="https://plus.google.com/+eleclockers-page"><i class="fa fa-google-plus-square fa-3x social"></i></a>
+            <a href="mailto:eleclockers@gmail.com"><i class="fa fa-envelope-square fa-3x social"></i></a>
         </div>
         <hr />
     </div>
--- resources/views/section.blade.php
+++ resources/views/section.blade.php
@@ -5,10 +5,16 @@
     </div>
 </div>
 <div class="container">
-    <div class="text-center center-block">
+    <div class="text-center center-block icon-block">
         <a href=""><i class="fa fa-home fa-5x"></i></a>
         <a href=""><i class="fa fa-lock fa-5x"></i></a>
         <a href=""><i class="fa fa-question fa-5x"></i></a>
         <a href=""><i class="fa fa-envelope fa-5x"></i></a>
     </div>
+    <!--<div class="text-center center-block icon-block">
+    <span class="iwt">
+    <span>Some longer sample text</span>
+    <i class="pull-right icon-chevron-right"></i>
+    </span>
+    </div>-->
 </div>
